3cdd0efa186e379689bf39e8481e81883d2f099c
pakhi <pakhivashis182001@gmail.com>
2022-08-24 18:32:25 +0530

    null pointer que

--- Pointers/Nullpointer.cpp
+++ Pointers/Nullpointer.cpp
@@ -5,9 +5,32 @@ int main(){
     //int *p = 0;
     //cout<<*p<<endl; 
     //when you try to access null pointer you will get segmentation fault
-    int i = 5;
+   /* int i = 5;
     int *p = &i;
     cout<<p<<endl;
     cout<<*p<<endl;
+    */
+   int num =5;
+   int a =num;
+   a++;
+   cout<<num<<endl;
+
+   int *p = &num;
+   cout<<"before"<<num<<endl;
+   (*p)++;
+   cout<<"after"<<num<<endl;
+
+   //copying a pointer
+   int *q=p;
+   cout<<p<<q<<endl;
+
+   //imp concept
+   int i = 3;
+   int *t = &i;
+   //cout<<(*t)++<<endl;
+   *t = *t + 1;
+   cout<<*t<<endl;
+
+
 
 }
