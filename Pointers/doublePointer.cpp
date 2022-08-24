3ad7abbd929876eeaa000c23e7e712a4e5a1d907
pakhi <pakhivashis182001@gmail.com>
2022-08-24 23:27:35 +0530

    double pointer

---
+++ Pointers/doublePointer.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+//pointers and functions
+void update(int **p2){
+    p2 = p2 + 1; //no change
+    
+
+}
+
+int main(){
+    int i =5;
+    int *p = &i;
+    int **p2 = &p;
+
+   /* cout<<p<<endl; //address and value of p
+    cout<<&p<<endl;
+
+    cout<<*p2<<endl; //same as of p
+    cout<<**p2<<endl; //to access value of p
+    */
+
+   cout<<endl;
+   
+
+    
+
+
+
+}
--- Pointers/function.cpp
+++ Pointers/function.cpp
@@ -9,14 +9,27 @@ void update(int *p){
     p = p+1;
 }
 
+int getSum(int *arr, int n){
+    cout<<endl<<"size:"<<sizeof(arr)<<endl;
+
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
 int main(){
 
-    int value = 5;
+   /* int value = 5;
     int *p = &value;
     //print(p);
 
     cout<<p<<endl;
     update(p); //before update and after remain same because only value can be updated
+    */
+   int arr[6]={1,2,3,4,5,6};
+   cout<<"sum:"<<getSum(arr+3,3)<<endl;
 
 
 }
