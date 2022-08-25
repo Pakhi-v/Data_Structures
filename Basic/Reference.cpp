d65b3543181229a19d9f1482f4966e169dc656a2
pakhi <pakhivashis182001@gmail.com>
2022-08-25 22:31:48 +0530

    pass by value

--- Basic/Reference.cpp
+++ Basic/Reference.cpp
@@ -1,7 +1,18 @@
 //reference variables have same memory but different names
 #include<iostream>
 using namespace std;
+
+void update1(int &n){ //pass by ref.
+    n++;
+}
+
+void update(int n){ //pass by value
+    n++;
+}
+
 int main(){
+
+    /*
     int i = 5;
     //create a ref variable
     int &j = i;
@@ -10,4 +21,14 @@ int main(){
     cout<<i<<endl;
     j++;
     cout<<i<<endl;
+    */
+
+   int n = 5;
+   cout<<"before"<<n<<endl;
+   update1(n); 
+   cout<<"after"<<n<<endl; 
+
+
+
+
 }
