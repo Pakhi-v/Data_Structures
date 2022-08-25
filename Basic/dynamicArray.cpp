c652a5ed9b2cddd6c1b99ea88f071dd7a566b325
pakhi <pakhivashis182001@gmail.com>
2022-08-25 22:50:02 +0530

    dynamic array

---
+++ Basic/dynamicArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int *arr){
+    int sum = 0;
+    for(int i = 0 ; i<n;i++){
+
+    }
+}
+
+
+int main(){
+   /* char ch = 'q';
+    cout<<sizeof(ch)<<endl;
+
+    char *c = &ch;
+    cout<<sizeof(c)<<endl;
+    */
+
+   int n;
+   cin>>n;
+
+   int *arr= new int[n]; //variable size array
+
+   for(int i = 0; i<n;i++){ //taking input array
+    cin>>arr[i];
+   }
+    
+}
