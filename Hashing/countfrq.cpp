e60449cd0e85a46c6b72781b3978bdb7e6519c73
pakhi <pakhivashis182001@gmail.com>
2022-09-05 12:09:32 +0530

    using map

--- Hashing/countfrq.cpp
+++ Hashing/countfrq.cpp
@@ -2,8 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-int main(){
-
+void frequency(int arr[],int n){
+    unordered_map<int,int> map;
+    for(int i=0;i<n;i++)
+     map[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto x : map)
+        cout << x.first << " " << x.second << endl;
+}
+ 
+int main()
+{
+    int arr[] = {10,5,10,15,10,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    frequency(arr, n);
+    return 0;
 }
