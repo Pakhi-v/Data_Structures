d1a62a3d08ec864165dd8fe3e5aab8cc05484e84
pakhi <pakhivashis182001@gmail.com>
2022-10-05 22:39:42 +0530

    head tail insertion in linked list

--- LinkedList/linked.cpp
+++ LinkedList/linked.cpp
@@ -6,18 +6,48 @@ class Node {
     int data;
     Node* next;
 
-    Node(int data){
+    Node(int data){  //constructor
         this -> data = data;
         this -> next = NULL;
     }
 };
 
+void insertathead(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertattail(Node* &tail, int d){
+     Node* temp = new Node(d);
+     tail -> next = temp;
+     tail = tail -> next;
+}
+
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp -> data<<" ";
+        temp = temp -> next;
+
+    }
+    cout<<endl;
+}
+
 
 int main(){
-
     Node* node1 = new Node(10);
-    cout<<node1 -> data << endl;
-    cout<<node1 -> next << endl;
+    //cout<<node1 -> data << endl;
+    //cout<<node1 -> next << endl;
 
+    Node* head = node1;
+    Node* tail = node1;
+    print(head);
 
+    insertattail(tail, 12);
+
+    print(head);
+    insertattail(tail, 15);
+    print(head);
 }
