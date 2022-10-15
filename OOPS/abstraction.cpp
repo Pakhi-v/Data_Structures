c0a7ff884208776cc909409f63be5bf03be4f4ba
pakhi <pakhivashis182001@gmail.com>
2022-10-15 12:41:58 +0530

    abstraction program

---
+++ OOPS/abstraction.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+class abstraction { 
+    private: 
+    int a, b; 
+    public: 
+    // method to set values of private members 
+    void set(int x, int y) { 
+        a = x; 
+        b = y; 
+        } 
+        void display() 
+        { 
+            cout << "a = " << a << endl; 
+            cout << "b = " << b << endl; 
+            } 
+            }; 
+            int main() { 
+                implementAbstraction obj; 
+                obj.set(10, 20); 
+                obj.display(); 
+                return 0; 
+                }
