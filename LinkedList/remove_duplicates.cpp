758dd9dccaebf602eedf17d677165d1f8a952586
pakhi <pakhivashis182001@gmail.com>
2022-11-17 23:07:18 +0530

    remove duplicates from sorted array

---
+++ LinkedList/remove_duplicates.cpp
@@ -0,0 +1,23 @@
+//remove duplicates from sorted array
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {        
+        if(head==nullptr)
+            return head;
+        
+        ListNode *temp=head;
+        
+        while(temp){
+            
+                if(temp->next != nullptr and temp->val == temp->next->val){
+                    temp->next = temp->next->next;
+                }
+                else{
+                    temp=temp->next;
+                }    
+        }
+
+        return head;
+        
+    }
+};
