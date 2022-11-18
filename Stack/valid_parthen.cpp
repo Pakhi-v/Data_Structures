ee5cdbe5466f8026710632ae9fca83375c7da37f
pakhi <pakhivashis182001@gmail.com>
2022-11-18 23:19:11 +0530

    valid parenthsis program

---
+++ Stack/valid_parthen.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack <char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            
+            else{
+                if(!st.empty()){
+                    if(s[i]==')'){
+                        if(st.top()=='(')
+                            st.pop();
+                        else
+                            return false;
+                    }
+                    if(s[i]=='}'){
+                        if(st.top()=='{')
+                            st.pop();
+                        else
+                            return false;
+                    }
+                    if(s[i]==']'){
+                        if(st.top()=='[')
+                            st.pop();
+                        else
+                            return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+            
+        }
+            
+            
+        if(st.empty())
+            return true;
+        
+        return false;
+        
+    }
+};
