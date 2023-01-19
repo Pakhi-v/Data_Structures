6808a3077786f29f17f1a7077e6ff8a60336a6dc
pakhi <pakhivashis182001@gmail.com>
2023-01-19 20:42:42 +0530

    hash set chaining techq

--- LinkedList/hash_set.cpp
+++ LinkedList/hash_set.cpp
@@ -19,3 +19,39 @@ int size;
         return m[key];
     }
 };
+
+//use of hashing function i.e chaining for larger range
+//we will use doubly LL
+
+/*
+vector<list<int>> m;
+int size;
+MyHashSet(){
+  size=1e6+1;
+  m.resize(size);
+}
+int hash(int key){
+    return key%size;
+}
+list<int> :: iterator search(int key){
+    int i=hash(key);
+    return find(m[i].begin(),m[i].end(),key);
+}
+void add(int key){
+    if(contains(key))return;
+    int i = hash(key);
+    m[i].push_back(key);
+}
+void remove(int key){
+    if(!contains(key))return;
+    int i=hash(key);
+    m[i].erase(search(key));
+}
+bool contains(int key){
+    int i=hash(key);
+    if(search(key) != m[i].end()) return true;
+    else return false;
+}
+};
+
+*/
