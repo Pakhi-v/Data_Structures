781c214666e76c50ac2ec238eb6e3ba047cb0e38
pakhi <pakhivashis182001@gmail.com>
2023-05-07 10:24:01 +0530

    print left view of binary tree

---
+++ BinaryTree/left_view_bt.cpp
@@ -0,0 +1,35 @@
+//method 1 : recursive(Pre-order traversal)
+int maxlevel = 0;
+void printleft(node *root, int level){
+    if(root == NULL) return;
+    if(maxlevel < level){
+        cout<<(root -> key)<<" ";
+        maxlevel = level;
+    }
+    printleft(root->left, level+1);
+    printleft(root->right,level + 1);
+}
+void printleftview(Node*root){
+    printleft(root,1);
+}
+
+//method 2 : iterative solution using queues
+void printleft(Node* root){
+    if(root == NULL) return;
+    queue <node*> q;
+    q.push(root);
+    while(q.empty() == false){
+        int count = q.size();
+        for(int i = 0;i <count ; i++)
+        {
+            Node* curr = q.top();
+            q.pop();
+            if(i == 0)
+            cout<<(curr->key)<<"";
+            if(curr->left!=NULL)
+            q.push(curr->left);
+            if(curr->right != NULL)
+            q.push(curr->right);
+        }
+    }
+}
