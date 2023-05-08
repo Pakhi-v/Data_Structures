f6ad310480c793c2c4fe7f71f7c255efbb94282a
pakhi <pakhivashis182001@gmail.com>
2023-05-08 15:52:40 +0530

    changes

--- BinaryTree/check_for_balanced_tree.cpp
+++ BinaryTree/check_for_balanced_tree.cpp
@@ -8,3 +8,13 @@ bool isbalanced(Node* root){
 
 
 //efficient solution
+int isbalanced(node* root){
+    if(root==NULL) return 0;
+    int lh = isbalanced(root->left);
+    if(lh == -1) return -1;
+    int rh = isbalanced(root->right);
+    if(rh == -1) return -1;
+    if(abs(lh-rh) > 1)
+    return -1;
+    return max(lh,rh)+1;
+}
