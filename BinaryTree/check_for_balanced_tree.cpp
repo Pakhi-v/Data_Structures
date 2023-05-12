ad5617992c2e5d27b2e98ffd8060a6ccf0fc34a3
pakhi <pakhivashis182001@gmail.com>
2023-05-12 16:05:34 +0530

    check for balanced tree program

--- BinaryTree/check_for_balanced_tree.cpp
+++ BinaryTree/check_for_balanced_tree.cpp
@@ -18,3 +18,41 @@ int isbalanced(node* root){
     return -1;
     return max(lh,rh)+1;
 }
+
+//other solution
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    int checkbalance(Node* root){
+         if(!root->left and !root->right) {
+            return 1;
+    }
+    int lh=0,rh=0;
+    if(root->left){
+        lh=checkbalance(root->left);
+        if(lh==-1){
+            return -1;
+        }
+    }
+    if(root->right){
+        rh=checkbalance(root->right);
+        if(rh==-1){
+            return -1;
+        }
+    }
+    if(abs(lh-rh)>1){
+        return -1;
+    }
+    return max(lh,rh) + 1;
+    }
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+       int ans = checkbalance(root);
+       if(ans == -1){
+           return 0;
+       }
+       return 1;
+        
+    }
+};
