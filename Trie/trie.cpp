632f57226d2f746bdff3fbeee70f92d5c2f4a220
pakhi <pakhivashis182001@gmail.com>
2023-06-13 11:53:10 +0530

    basic rep of trie

--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -1,1 +1,10 @@
 //This is basic representation of trie data structure
+struct TrieNode{
+    TrieNode* child[26];
+    bool isEnd;
+    TrieNode(){
+        isEnd = false;
+        for(int i=0;i<26;i++)
+            child[i] = NULL;
+    }
+}
