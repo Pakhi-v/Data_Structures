4a4de7073179a1f312a5c81e34c2be59f4523142
pakhi <pakhivashis182001@gmail.com>
2023-06-14 11:37:13 +0530

    greedy problems

---
+++ Greedy/fractional_knap.cpp
@@ -0,0 +1,9 @@
+//comparsion function
+bool cmp(pair<int,int> a, pair<int,int> b){
+    double r1 = (double)a.first/a.second;
+    double r2 = (double)b.first/b.second;
+
+    return r1>r2;
+
+}
+//main function
