3b4acb915c29775768f8345c21b156910274512e
pakhi <pakhivashis182001@gmail.com>
2023-06-14 11:44:29 +0530

    fractional knapstack problem

--- Greedy/fractional_knap.cpp
+++ Greedy/fractional_knap.cpp
@@ -1,5 +1,5 @@
 //comparsion function
-bool cmp(pair<int,int> a, pair<int,int> b){
+bool mcmp(pair<int,int> a, pair<int,int> b){
     double r1 = (double)a.first/a.second;
     double r2 = (double)b.first/b.second;
 
@@ -7,3 +7,18 @@ bool cmp(pair<int,int> a, pair<int,int> b){
 
 }
 //main function
+double fkanps(int W, pair<int,int> arr[], int n){
+    sort(arr,arr+n,mycmp);
+    double res = 0.0;
+    for(int i=0;i<n;i++){
+        if(arr[i].second <= W){
+            res + = arr[i].first;
+            W = W - arr[i].second;
+        }
+        else{
+            res + = arr[i].first * ((double) W / arr[i].second);
+            break;
+        }
+    }
+    return res;
+}
